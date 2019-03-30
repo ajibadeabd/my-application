24759a0fa99b95918a23d01e5f43bbcbb23fed9a
ajibadeabd <ajibadeabd@gmail.com>
2019-03-30 21:15:07 +0100

    my second commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 document.getElementById('send').onclick = () =>{
-    document.getElementById('para').innerHTML='thank for clicking'
+    document.getElementById('para').innerHTML='thank for clicking';
+    document.getElementById('send').style.color='red';
+    document.getElementById('header').style.color='red'
 }
